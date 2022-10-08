5e3d30a0b019abfb714d953a8281a56e253ae01a
Suryaputhran13 <manukmpranoy@gmail.com>
2022-10-08 13:21:53 +0000

    yes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -38,9 +38,9 @@ const Hero = () => {
         {/* gradient end */}
       </div>
 
-      <div className={`ss:hidden ${styles.flexCenter}`}>
+     {/* <div className={`ss:hidden ${styles.flexCenter}`}>
         <GetStarted />
-      </div>
+        </div> */}
     </section>
   );
 };
