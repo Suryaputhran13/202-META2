90285626bff619a6d233571102b80cea214e9e75
Suryaputhran13 <manukmpranoy@gmail.com>
2023-01-04 07:20:46 +0000

    update

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -13,7 +13,7 @@ const Testimonials = () => (
       <div className="w-full md:mt-0 mt-6">
         <h1 className={`${styles.heading2} text-left margin-left 10 em  max-w-[1500px]`}>About 202 META</h1>
         <p className={`${styles.paragraph} text-centre  max-w-[1500px]`}>
-        202 META is committed to making a positive & sustainable impact on the global community and the environment they are a part of through industry leading technology. At 202 META we will plan to provide our community with a new experience of Metaverse through NFTs , Play to Earn Games , Crypto mining and also swapping different tokens through our decentralized exchange platform. In order to make our platform more cost effective we use EOSIO Blockchain based technologies and smart contracts so that transcation fees inside the network is zero.
+        202 META is committed to making a positive & sustainable impact on the global community and the environment they are a part of through industry leading technology. At 202 META we plan to provide our community with a new experience of Metaverse through NFTs , Play to Earn Games , Crypto mining and also swapping different tokens through our decentralized exchange platform. In order to make our platform more cost effective we use EOSIO Blockchain based technologies and smart contracts so that transcation fees inside the network is zero.
         </p>
       </div>
     </div>
